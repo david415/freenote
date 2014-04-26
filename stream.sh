25ccdd931a02ee6f881f44df421f0796976b36d1
David Stainton <dstainton415@gmail.com>
2014-04-26 15:07:30 +0000

    Cleanup gstreamer pipeline

--- stream.sh
+++ stream.sh
@@ -5,7 +5,7 @@ set -ex
 main() {
   # Your audio device
   # Find the correct audio device with with `pactl list`
-  : ${AUDIODEVICE:=alsa_input.usb-046d_HD_Pro_Webcam_C920_6DBE3ADF-02-C920.analog-stereo}
+  : ${AUDIODEVICE:=alsa_input.usb-046d_0825_6879E360-02-U0x46d0x825.analog-mono}
   : ${VIDEODEVICE:=/dev/video0}
   #
   # Assuming that you're authorized by the FSF - the video should be available
@@ -13,15 +13,8 @@ main() {
   #
   #   http://live.fsf.org/keynote.ogv
   #
-  : ${SERVER:=live.fsf.org}
-  : ${IP:=18.4.89.36}
-  : ${PORT:=80}
-  : ${MOUNTPOINT:=/keynote.ogv}
-  # Replace this with the correct password, of course
-  : ${PASSWORD:=GnUisL1ve}
   : ${LOCALIP:=127.0.0.1}
   : ${LOCALPORT:=8080}
-  : ${OVERLAYTEXT:="Anonymously streamed over Tor"}
   : ${FRAMERATE:=10}
   echo $AUDIODEVICE
   stream
@@ -32,14 +25,11 @@ stream() {
   usewithtor gst-launch-0.10 \
   v4l2src ! \
   video/x-raw-yuv,device="$VIDEODEVICE",width="640,height=480,framerate=(fraction)$FRAMERATE/1" ! queue ! \
-  textoverlay shaded-background=true text="$OVERLAYTEXT" !\
   ffmpegcolorspace ! tee name=localview ! theoraenc ! queue ! \
-  oggmux name=mux pulsesrc device="$AUDIODEVICE" ! queue ! \
-  audioconvert ! vorbisenc ! queue ! mux. mux. ! queue ! \
-  tee name=shout ! tee name=fileout ! tcpserversink  host="$LOCALIP" \
-  port="$LOCALPORT" shout. ! queue ! \
-  shout2send ip="$IP" port="$PORT" password="$PASSWORD" mount="$MOUNTPOINT" \
-  fileout. ! queue ! filesink sync=false location="video-`date +%s`.ogv" \
+  oggmux name=mux \
+  pulsesrc device="$AUDIODEVICE" ! queue ! \
+  audioconvert ! vorbisenc ! queue ! mux. \
+  mux. ! queue ! tcpserversink  host="$LOCALIP" port="$LOCALPORT" \
   localview. ! queue ! xvimagesink sync=false
 }
 
